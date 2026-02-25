11616092d6449abf00dd6e3b3358061068594dc8
InuKazuki <vinicius.silm17@gmail.com>
2026-02-25 20:55:44 -0300

    retirando imports desnecessários

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
-  const isAuthenticated = !!localStorage.getItem("token");
-
   return (
     <BrowserRouter>
       <Routes>
